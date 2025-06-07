039aa073e340c97554d65501ed065a56335348aa
alexgrauroca <alexgrauroca91@gmail.com>
2025-06-07 18:10:46 +0200

    Repo setup: created docker containers for mongodb and auth-service

--- services/authentication-service/internal/customers/repository.go
+++ services/authentication-service/internal/customers/repository.go
@@ -4,8 +4,12 @@ import (
 	"context"
 	"time"
 
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
 	"go.uber.org/zap"
 )
+
+const customersCollection = "customers"
 
 //go:generate mockgen -destination=./mocks/repository_mock.go -package=mocks github.com/alexgrauroca/practice-food-delivery-platform/services/authentication-service/internal/customers Repository
 type Repository interface {
@@ -19,25 +23,42 @@ type CreateCustomerParams struct {
 }
 
 type Customer struct {
-	ID        string
-	Email     string
-	Name      string
-	CreatedAt time.Time
-	UpdatedAt time.Time
-	Active    bool
+	ID        string    `bson:"_id,omitempty"`
+	Email     string    `bson:"email"`
+	Name      string    `bson:"name"`
+	CreatedAt time.Time `bson:"created_at"`
+	UpdatedAt time.Time `bson:"updated_at"`
+	Active    bool      `bson:"active"`
 }
 
 type repository struct {
-	logger *zap.Logger
+	logger     *zap.Logger
+	collection *mongo.Collection
 }
 
-func NewRepository(logger *zap.Logger) Repository {
+func NewRepository(logger *zap.Logger, db *mongo.Database) Repository {
 	return &repository{
-		logger: logger,
+		logger:     logger,
+		collection: db.Collection(customersCollection),
 	}
 }
 
 func (r *repository) CreateCustomer(ctx context.Context, params CreateCustomerParams) (Customer, error) {
-	//TODO implement me
-	panic("implement me")
+	now := time.Now()
+
+	c := Customer{
+		Email:     params.Email,
+		Name:      params.Name,
+		CreatedAt: now,
+		UpdatedAt: now,
+		Active:    true,
+	}
+	res, err := r.collection.InsertOne(ctx, c)
+	if err != nil {
+		r.logger.Error("Failed to insert customer", zap.Error(err))
+		return Customer{}, err
+	}
+	c.ID = res.InsertedID.(primitive.ObjectID).Hex()
+	r.logger.Info("Customer created successfully", zap.String("customer_id", c.ID))
+	return c, nil
 }
