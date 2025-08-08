4dc52da780e0f9cb0d8a85e14faa158dbd2e0dd4
alexgrauroca <alexgrauroca91@gmail.com>
2025-08-08 17:37:15 +0200

    clients auto generator implementation

--- scripts/install-oapi-codegen.sh
+++ scripts/install-oapi-codegen.sh
@@ -3,5 +3,5 @@ set -e
 
 if ! command -v oapi-codegen &> /dev/null; then
   echo "oapi-codegen not found, installing..."
-  go install github.com/deepmap/oapi-codegen/cmd/oapi-codegen@latest
+  go install github.com/oapi-codegen/oapi-codegen/v2/cmd/oapi-codegen@latest
 fi
