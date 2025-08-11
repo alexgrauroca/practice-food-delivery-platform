5902c70a662e410ac6eb2295d8ee22fd4448c279
alexgrauroca <alexgrauroca91@gmail.com>
2025-08-11 16:57:24 +0200

    setting up main file for customer service

---
+++ services/customer-service/internal/middleware/requestinfo.go
@@ -0,0 +1,36 @@
+// Package middleware provides HTTP middleware components for handling cross-cutting
+// concerns in the authentication service
+package middleware
+
+import (
+	"github.com/gin-gonic/gin"
+	"github.com/google/uuid"
+
+	"github.com/alexgrauroca/practice-food-delivery-platform/services/customer-service/internal/log"
+)
+
+// RequestInfoMiddleware is a middleware that attaches request-specific information to the context.
+func RequestInfoMiddleware() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		requestID := c.GetHeader("X-Request-ID")
+		if requestID == "" {
+			requestID = uuid.New().String()
+		}
+
+		realIP := c.GetHeader("X-Real-IP")
+		if realIP == "" {
+			realIP = c.ClientIP()
+		}
+
+		info := log.RequestInfo{
+			RequestID: requestID,
+			Host:      c.Request.Host,
+			RealIP:    realIP,
+			UserAgent: c.Request.UserAgent(),
+		}
+
+		ctx := log.WithRequestInfo(c.Request.Context(), info)
+		c.Request = c.Request.WithContext(ctx)
+		c.Next()
+	}
+}
