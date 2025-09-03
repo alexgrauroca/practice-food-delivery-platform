ffedcbbdefed5f64aec9133c98dcfc46c0a5c75b
alexgrauroca <alexgrauroca91@gmail.com>
2025-09-03 07:54:07 +0200

    Unique key generator with atomic integer + docker logs improvement

--- e2e/domain/customer/factory.go
+++ e2e/domain/customer/factory.go
@@ -3,11 +3,17 @@ package customer
 
 import (
 	"fmt"
+	"sync/atomic"
 	"time"
 )
 
+var counter atomic.Int64
+var uniqueKey string
+
 // New creates and returns a new TestCustomer with predefined and dynamically generated fields.
 func New() TestCustomer {
+	uniqueKey = fmt.Sprintf("%d_%d", time.Now().Nanosecond(), counter.Add(1))
+
 	return TestCustomer{
 		Email:       generateEmail(),
 		Password:    "strongpassword123",
@@ -20,9 +26,9 @@ func New() TestCustomer {
 }
 
 func generateEmail() string {
-	return fmt.Sprintf("e2e_test_user_%d@example.com", time.Now().UnixNano())
+	return "e2e_test_user_" + uniqueKey + "@example.com"
 }
 
 func generateName() string {
-	return fmt.Sprintf("E2E Test User%d", time.Now().UnixNano())
+	return "E2E Test User " + uniqueKey
 }
