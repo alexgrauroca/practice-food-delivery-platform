17ddba0c2f343dd2f5a9e852badb2af4e6fbd8e4
alexgrauroca <alexgrauroca91@gmail.com>
2025-10-28 20:20:22 +0100

    Handler implementation + small errors refactor

--- services/authentication-service/internal/authcore/errors.go
+++ services/authentication-service/internal/authcore/errors.go
@@ -16,4 +16,14 @@ const (
 	CodeInvalidCredentials = "INVALID_CREDENTIALS"
 	// MsgInvalidCredentials represents the error message returned when login authentication fails due to invalid credentials.
 	MsgInvalidCredentials = "invalid credentials"
+
+	// CodeInvalidRefreshToken represents the error code for an invalid or expired refresh token used in authentication processes.
+	CodeInvalidRefreshToken = "INVALID_REFRESH_TOKEN"
+	// MsgInvalidRefreshToken represents an error message indicating an invalid or expired refresh token.
+	MsgInvalidRefreshToken = "invalid or expired refresh token"
+
+	// CodeTokenMismatch represents an error code indicating a mismatch between the provided token and the expected value.
+	CodeTokenMismatch = "TOKEN_MISMATCH"
+	// MsgTokenMismatch represents the error message for a token mismatch scenario.
+	MsgTokenMismatch = "token mismatch"
 )
